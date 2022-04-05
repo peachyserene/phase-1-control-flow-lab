373e7ea16b5e16a50e69d3bef539304ce7c6ee4e
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-04-04 20:38:58 -0400

    commit tree 326d979109171fe3f8526363be4e04825769368c

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetTraveling){
+  if (feetTraveling <= 400){
+    return 'This one is on me!';
+  }
+  else if (feetTraveling <= 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (feetTraveling <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feetTraveling > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const rideAvailable = city == "NYC" ? 'Ok, sounds good.' : 'No go.';
+  return rideAvailable;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipQuality){
+  let response;
+
+  switch (tipQuality) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
